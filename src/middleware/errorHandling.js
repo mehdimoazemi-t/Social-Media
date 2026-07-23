0c7880256c8c1f18f671669af554e79d445afdda
mehdi <mehdimoazemi.t@gmail.com>
2026-07-23 18:55:54 +0400

    refactor: improve Joi error handling middleware with switch/case

--- src/middleware/errorHandling.js
+++ src/middleware/errorHandling.js
@@ -1,7 +1,7 @@
 const { errorRespone } = require("../utils/response");
 
 const errorHandling = (schema) => {
-    
+
     return async (req, res, next) => {
 
         try {
@@ -11,12 +11,61 @@ const errorHandling = (schema) => {
             if (error) {
 
                 const errObject = error.details.reduce((initialValue, currentValue) => {
-                    initialValue[currentValue.path[0]] = currentValue.message
+
+                    let errType = currentValue.type
+
+                    switch (errType) {
+
+                        case "string.email":
+                            initialValue[currentValue.path[0]] = "Please enter a valid email address."
+
+                            break;
+
+                        case "any.required":
+                            initialValue[currentValue.path[0]] = "The field is required and has not been filled in."
+
+                            break;
+
+                        case "any.required":
+                            initialValue[currentValue.path[0]] = "The field is required and has not been filled in."
+
+                            break;
+
+                        case "string.min":
+                            initialValue[currentValue.path[0]] = "The string length is less than the allowed limit."
+
+                            break;
+
+                        case "number.base":
+                            initialValue[currentValue.path[0]] = "The input must be a number."
+
+                            break;
+
+
+                        case "number.base":
+                            initialValue[currentValue.path[0]] = "The input must be a number."
+
+                            break;
+
+
+                        case "any.only":
+                            initialValue[currentValue.path[0]] = "Please select a valid value"
+
+                            break;
+
+
+                        default:
+
+                            initialValue[currentValue.path[0]] = currentValue.message
+
+                    }
+
                     return initialValue
+                    
                 }, {})
 
                 return errorRespone(res, 400, {
-                    error: errObject 
+                    error: errObject
                 })
             }
 
