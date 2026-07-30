ba363cec00b03a50408f8d25e3a7db59a621b3dd
mehdi <mehdimoazemi.t@gmail.com>
2026-07-30 16:47:01 +0330

    feat(post): create post mongoose model

---
+++ src/models/Post.js
@@ -0,0 +1,32 @@
+const { required } = require("joi")
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    media: {
+        path: { type: String },
+        filename: { type: String }
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    like: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    tags: {
+        type: [String],
+        required: false
+    }
+})
+
+const model = mongoose.model("Post", schema)
+
+module.exports = model
