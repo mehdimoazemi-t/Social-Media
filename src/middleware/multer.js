d8a1f2bfb652c31522b943c07a3fabc7890bf114
mehdi <mehdimoazemi.t@gmail.com>
2026-07-30 18:22:08 +0330

    feat(upload): add multer utility with dynamic directory creation and file type filtering

---
+++ src/middleware/multer.js
@@ -0,0 +1,44 @@
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+
+const multerUploader = ( destination, allowedTypes = /jpg|jpeg|png|WebP/) => {
+
+    if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination)
+    }
+
+    const storage = multer.diskStorage({
+
+        destination: function (req, file, cb) {
+            cb(null, destination)
+        },
+        filename: function (re, file, cb) {
+            const fileName = Date.now() + Math.floor(Math.random() * 9999999)
+            const ext = path.extname(file.originalname)
+            cb(null, `${fileName}${ext}`)
+        }
+    })
+
+
+    const fileFilter = (req, file, cb) => {
+        const type = file.mimetype
+
+        if (allowedTypes.test(type)) {
+            cb(null, true)
+        }
+        cb(new Error("Invalid file type. Only JPG, JPEG, PNG, and WebP are allowed"), false)
+
+    }
+
+
+    const upload = multer({
+        storage,
+        fileFilter
+    })
+
+    return upload
+}
+
+
+module.exports = multerUploader
