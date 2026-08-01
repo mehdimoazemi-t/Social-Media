088f087b46f5738e0fccb2e9e06b4b536e95841f
mehdi <mehdimoazemi.t@gmail.com>
2026-08-01 16:22:54 +0330

    feat: implement post upload     - integrate multer for image/video upload     - add validation for post fields     - connect post creation to mongodb

--- src/module/post/postController.js
+++ src/module/post/postController.js
@@ -1,4 +1,39 @@
+const postModel = require("../../models/Post")
 
 exports.showViewUploader = (req, res) => {
     return res.render("post/upload")
+}
+
+
+exports.createPost = async (req, res, next) => {
+
+    try {
+        const { description, tags } = req.body
+
+        const hashtags = tags.split(",")
+        const mediaUrlPath = `post/${req.file?.filename}`
+
+        if (!req.file) {
+            req.flash("error", "media is required")
+            return res.render("post/upload")
+        }
+
+        let post = new postModel({
+            description,
+            tags: hashtags,
+            user: req.user._id,
+            media: {
+                path: mediaUrlPath,
+                filename: req.file.originalname
+            }
+        })
+
+        post = await post.save()
+
+        req.flash("success", "Post Uploaded Successfully")
+        return res.render("post/upload")
+
+    } catch (error) {
+        next(error)
+    }
 }
--- src/module/post/postRouter.js
+++ src/module/post/postRouter.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const postController = require("./postController")
 const { accountVerify } = require("../../middleware/accountVerify")
+const path = require("path")
 const router = express.Router()
+const multerUploader = require("../../middleware/multer")
 const auth = require("../../middleware/auth")
+const errorHandling = require("../../middleware/errorHandling")
+const postValidateSchema = require("./postValidator")
 
+
+const destinationPost = path.join(__dirname, "..", "..", "..", "public", "post");
+const allowedTypes = /jpg|jpeg|png|WebP|mkv|mp4/
+
+const uploader = multerUploader(destinationPost, allowedTypes)
 
 router.route("/upload")
     .get(auth, accountVerify, postController.showViewUploader)
+    .post(auth, errorHandling(postValidateSchema, "/post/upload"), uploader.single("media"), postController.createPost)
 
 
 module.exports = router
